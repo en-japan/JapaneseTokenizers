ed7ec3863933253081c0cc38806acae2d4a09164
Ugo Bataillard <ugo_bataillard@en-japan.com>
2016-01-27 10:33:47 +0900

    Fixed packaging

--- JapaneseTokenizer/__init__.py
+++ JapaneseTokenizer/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'kensuke-mi'
-__version__ = 0.5
+__version__ = '0.5'
 
 from mecab_wrapper import MecabWrapper
 from JapaneseTokenizer.datamodels import TokenizedSenetence
--- setup.py
+++ setup.py
@@ -1,41 +1,21 @@
 #! -*- coding: utf-8 -*-
+import codecs
+import os 
+import re
 from setuptools import setup, find_packages
-import JapaneseTokenizer.mecab_wrapper.mecab_wrapper as mecab_wrapper
-import sys
-from JapaneseTokenizer import __version__
-import install_python_dependencies
 
-python_version = sys.version_info
-
-
-install_requires = ['mecab-python']
-
-try:
-    import MeCab
-    mecabObj = MeCab.Tagger('-Ochasen')
-    if python_version >= (3, 0, 0):
-        text = '本日は晴天なり'
-    else:
-        text = u'本日は晴天なり'.encode('utf-8')
-    node = mecabObj.parseToNode(text)
-    node = node.next
-    while node.next is not None:
-        if python_version >= (3, 0, 0):
-            word_surface = node.surface
-        else:
-            word_surface = node.surface.decode('utf-8')
-        node = node.next
-except Exception as e:
-    print(e)
-    sys.exit("Mecab and Mecab-python is not ready to use. Please setup first")
-
+# avoid a from JapaneseTokenizer import __version__ as version (that compiles JapaneseTokenizer.__init__ and is not compatible with bdist_deb)
+version = None
+for line in codecs.open(os.path.join('JapaneseTokenizer', '__init__.py'), 'r', encoding='utf-8'):
+    matcher = re.match(r"""^__version__\s*=\s*['"](.*)['"]\s*$""", line)
+    version = version or matcher and matcher.group(1)
 
 setup(
     author='Kensuke Mitsuzawa',
     name = 'JapaneseTokenizer',
-    version=__version__,
+    version=version,
     test_suite='JapaneseTokenizer.test.test_all.suite',
-    install_requires = install_requires,
+    install_requires = ['mecab-python'],
     packages=find_packages(),
     )
 
